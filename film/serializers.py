2392d0d35a8c06805b4ff562da53ccb7ab3dbbcc
Amirxon1301 <uchihasaske0102@gmail.com>
2023-12-22 15:48:47 +0500

    Netflix loyihasidagi uyga vazifalar va yangi darsda qilingan narsalar

--- film/serializers.py
+++ film/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from .models import *
+from rest_framework.exceptions import ValidationError
 
 class AktyorSerializer(serializers.Serializer):
     id = serializers.IntegerField()
@@ -6,3 +8,42 @@ class AktyorSerializer(serializers.Serializer):
     davlat = serializers.CharField()
     jins = serializers.CharField()
     tugilgan_yil = serializers.DateField()
+
+    def validate_ism(self, qiymat):
+        if len(qiymat) < 4:
+            raise ValidationError("Ism-familiya bunday kalta bo'lmaydi")
+        return qiymat
+
+    def validate_jins(self, qiymat):
+        if qiymat != 'Erkak' and qiymat != 'Ayol':
+            raise ValidationError("Xato")
+        return qiymat
+
+
+class TarifSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    nom = serializers.CharField()
+    narx = serializers.IntegerField()
+    davomiylik = serializers.DurationField()
+
+class IzohSerializer(serializers.Serializer):
+    class Meta:
+        model = Izoh
+        fields = '__all__'
+class KinoSerializer(serializers.ModelSerializer):
+    aktyorlar = AktyorSerializer(many=True)
+    class Meta:
+        model = Kino
+        fields = '__all__'
+
+class KinoPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kino
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        kino = super(KinoSerializer, self).to_representation(instance)
+        kino.update({"aktyorlar_soni": len(kino.get("aktyorlar"))})
+        kino.update({"izoh_soni": instance.izoh_set.all().count()})
+        return kino
+
--- film/views.py
+++ film/views.py
@@ -1,5 +1,19 @@
+from rest_framework.filters import OrderingFilter
+from rest_framework import status
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import *
+
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+
+from rest_framework.pagination import PageNumberPagination
+from django.contrib.postgres.search import TrigramSimilarity
+
 from .models import *
 from .serializers import *
 
@@ -22,9 +36,19 @@ class HelloAPI(APIView):
 class AktyorlarAPI(APIView):
     def get(self, request):
         aktyorlar = Aktyor.objects.all()
-        serializer = AktyorSerializer(aktyorlar, many=True)
+        qidiruv = request.query_params.get("qidiruv")
+        if qidiruv:
+            aktyorlar = Aktyor.objects.annotate(
+                oxshashlik = TrigramSimilarity('ism', qidiruv)
+            ).filter(oxshashlik__gt=0.5).order_by("-oxshashlik")
+        pagination_class = PageNumberPagination
+        pagination_class.page_size = 1
+        paginator = PageNumberPagination()
+        natija = paginator.paginate_queryset(aktyorlar, request)
+        serializer = AktyorSerializer(natija, many=True)
 
         return Response(serializer.data)
+
 
     def post(self, request):
         aktyor = request.data
@@ -51,7 +75,7 @@ class AktyorAPI(APIView):
         serializer = AktyorSerializer(aktyor, data=request.data)
         if serializer.is_valid():
             data = serializer.validated_data
-            Aktyor.objects.filter().update(
+            Aktyor.objects.filter(id=pk).update (
                 ism = data.get("ism"),
                 davlat= data.get("davlat"),
                 jins= data.get("jins"),
@@ -59,5 +83,97 @@ class AktyorAPI(APIView):
             )
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+class TariflarAPI(APIView):
+    def get(self, request):
+        tarif = Tarif.objects.all()
+        serializer = TarifSerializer(tarif, many=True)
+
+        return Response(serializer.data)
+    def post(self, request):
+        tarif = request.data
+        serializer = TarifSerializer(data=tarif)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            Tarif.objects.create(
+                ism=data.get("ism"),
+                davlat=data.get("davlat"),
+                jins=data.get("jins"),
+                tugilgan_yil=data.get("tugilgan_yil"),
+            )
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class TarifAPI(APIView):
+    def delete(self, request, pk):
+        Tarif.objects.get(id=pk).delete()
+        return Response({"successful" : "True"})
+
+    def update(self, request, pk):
+        tarif = Tarif.objects.get(id=pk)
+        serializer = TarifSerializer(data=tarif)
+        if serializer.is_valid():
+            data = serializer.validated_data
+            Tarif.objects.filter(id=pk).update(
+                ism=data.get("ism"),
+                davlat=data.get("davlat"),
+                jins=data.get("jins"),
+                tugilgan_yil=data.get("tugilgan_yil"),
+            )
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class KinolarAPI(APIView):
+    def post(self, request):
+        kino = request.data
+        serializer = KinoPostSerializer(data=kino)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    def get(self, request):
+        kinolar = Kino.objects.all()
+        serializer = KinoSerializer(kinolar, many=True)
+        return Response(serializer.data)
+
+
+class KinoAPI(APIView):
+    def get(self, request, pk):
+        kino = Kino.objects.get(id=pk)
+        serializers = KinoSerializer(kino)
+        return Response(serializers.data)
+
+class IzohModelViewSet(ModelViewSet):
+    queryset = Izoh.objects.all()
+    serializer_class = IzohSerializer
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['id', 'sana' ]
+
+class IzohListCreateAPI(ListCreateAPIView):
+    serializer_class = IzohSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Izoh.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class IzohDeleteAPI(DestroyAPIView):
+    queryset = Izoh.objects.all()
+    serializer_class = IzohSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def perform_destroy(self, instance):
+        if instance.user == self.request.user:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
 
 
